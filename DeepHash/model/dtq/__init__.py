4c60e6891d1450306a8538f93da744081385c76a
sobhe <sobhe@sobhe.prg.tetra>
2019-09-08 18:07:02 +0430

    Add some codes to easily save triplets.

--- DeepHash/model/dtq/__init__.py
+++ DeepHash/model/dtq/__init__.py
@@ -6,6 +6,22 @@ def train(train_img, database_img, query_img, config):
     img_database = Dataset(database_img, config.output_dim, config.subspace * config.subcenter)
     img_query = Dataset(query_img, config.output_dim, config.subspace * config.subcenter)
     img_train = Dataset(train_img, config.output_dim, config.subspace * config.subcenter)
+    
+    
+    #img_train.update_triplets(config.triplet_margin, n_part=config.n_part, select_strategy=config.select_strategy)
+    #import os
+    #import numpy as np
+    #arr = img_train.triplets
+    #idx = img_train._perm[np.concatenate([arr[:, 0], arr[:, 1], arr[:, 2]], axis=0)]
+    #ds = img_train._dataset
+    #names = []
+    #for i in idx:
+    #    a = os.path.join(ds.data_root, ds.lines[i].strip().split()[0])
+    #    names.append(a)
+    #import pickle as pkl
+    #pkl.dump(names, open('/home/sobhe/sam/DeepHash/triples.pkl', 'wb'))
+    #np.save('/home/sobhe/sam/DeepHash/triplets.npy', idx)
+    
     model.train_cq(img_train, img_query, img_database, config.R)
     return model.save_dir
 
